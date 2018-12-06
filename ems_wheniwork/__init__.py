035da3f7795723b256bc95ef09eee455206c4bc4
Bradley Bell <bradleyb@uw.edu>
2018-12-06 15:38:07 -0800

    add EMS mock data

--- ems_wheniwork/__init__.py
+++ ems_wheniwork/__init__.py
@@ -2,6 +2,10 @@ import os
 from os.path import abspath, dirname
 
 from restclients_core.dao import MockDAO
+from ems_client.mock import EMSMockData
 
 MockDAO.register_mock_path(os.path.join(
+    abspath(dirname(__file__)), "resources"))
+
+EMSMockData.register_mock_path(os.path.join(
     abspath(dirname(__file__)), "resources"))
